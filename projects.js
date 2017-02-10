66341b2e40ea37fa534fd91f7faf71e8f691143d
rociohandal <rocio.faby14@gmail.com>
2017-02-10 15:14:34 -0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 /* Demo purposes only */
 $(".hover").mouseleave(
   function () {
-    $(this).removeClass("hover");// This is the javacode that instructs the imae that when you hover over it with the mouse a function will be activated//
+    $(this).removeClass("hover");
   }
 );
